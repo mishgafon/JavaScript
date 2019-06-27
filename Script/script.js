42cb31ab7269f8cb49ce46260c3351a8c5c09eff
Mikhail Volobuev <mwork07@mail.ru>
2019-06-27 23:10:58 +0400

    first commit

---
+++ Script/script.js
@@ -0,0 +1,14 @@
+let money = 10;
+console.log('money: ', money);
+const income = 200; 
+console.log('income: ', income);
+let addExpenses = 30;
+console.log('addExpenses: ', addExpenses);
+const deposit = 400;
+console.log('deposit: ', deposit);
+let mission = 50;
+console.log('mission: ', mission);
+let period = 2;
+console.log('period: ', period);
+alert("ввод данных окончен!");
+
