076c487d05ba7226e3504eccdce247f1d17a7031
Mikhail Volobuev <mwork07@mail.ru>
2019-07-10 23:08:12 +0400

    Урок №7. Исправления

--- Script/script.js
+++ Script/script.js
@@ -1,31 +1,39 @@
 'use strict';
+let body = document.querySelector('body'),
+	books = document.querySelectorAll('.books'),
+	book = document.querySelectorAll('.book'),
+	adv = document.querySelectorAll('.adv'),
+	ul = document.querySelectorAll('ul'), 
+	li = document.querySelectorAll('li'),
+	a = document.querySelectorAll('a'),
+	chapter8 = document.createElement('li');
 
-let books = document.querySelectorAll('.books'),
-    book = document.querySelector('.book'),
-    bookHead = document.querySelectorAll('a'),
-    bookItem = document.querySelectorAll('li'),
-    removeGoogle = document.querySelector('.adv'),
+    console.log(body, books, book, a, ul, li);   
     
-    body = document.querySelector('body');
-    
-    
-    //main = document.querySelectorAll('div');
-
-// console.log(books);
- console.log(book);
- console.log(bookItem);
-// //console.log(body);
-// console.log(body);
-// console.log(bookItem);
-//console.log(removeGoogle);
-books[0].insertBefore(book[1], book[0]);
+ 
+ 
+books[0].insertBefore(book[1], book[0]); // переставляем блоки с книгами
 books[0].insertBefore(book[2], null);
 books[0].insertBefore(book[4], book[3]);
+adv[0].remove(); // удаление рекламы 
 body.setAttribute('style', 'background-image: url(../image/you-dont-know-js.jpg)');
-bookHead[4].textContent = 'Книга 3. this и Прототипы Объектов';
-removeGoogle.classList.remove('adv');
-console.log(bookItem[6]);
-console.log(book[0]);
-book[2].insertBefore(bookItem[6], bookItem[4]);
+a[4].textContent = 'Книга 3. this и Прототипы Объектов';
+ul[0].insertBefore(li[8], li[4]); 
+ul[0].insertBefore(li[6], li[8]);
+ul[0].insertBefore(li[2], li[10]); 
+ul[5].insertBefore(li[48], li[52]);
+ul[5].insertBefore(li[55], li[49]);  
+ul[5].insertBefore(li[51], li[54]);
+chapter8.textContent = 'Глава 8: За пределами ES6';
+ul[2].appendChild(chapter8);
+ul[2].insertBefore(li[26], null);
 
-//book[5]
+
+
+
+
+
+
+
+
+
