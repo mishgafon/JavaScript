b76d0a41307fe918d234afb166b156d0c1f641e0
Mikhail Volobuev <mwork07@mail.ru>
2019-08-02 23:42:06 +0400

    урок №19. Правки

--- Script/script.js
+++ Script/script.js
@@ -399,7 +399,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
 		const checkText = () => {
 			document.body.addEventListener('input', (event) => {
-				if (event.target.matches ('.form-name', '#form2-name', '.mess') ) {
+				if (event.target.matches ('.form-name, #form2-name, .mess') ) {
 					event.target.value = event.target.value.replace(/[^а-я]/gi, '');
 		  		};
 			});
